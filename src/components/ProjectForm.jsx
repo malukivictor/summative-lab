4929763190081f6d523ae7e3f103cb9e1e4ac201
Maluki Victor <victormaluki0@gmail.com>
2026-09-02 12:24:16 +0300

    fixed disappeared search boxes with styling

--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -19,12 +19,12 @@ const ProjectForm = ({ onAddProject }) => {
 
   return (
     <div className='border-2 border-black rounded-lg m-4 p-6'>
-      <h2>Add project</h2>
+      <h2 className='text-lg font-bold mb-2'>Add project</h2>
       <form onSubmit={handleSubmit} className='flex flex-col gap-2'>
         <label>
           Title
           <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}
-          className='bg-white border border-black h-10'/>
+          className='bg-white border border-black rounded-md h-10 w-full px-2'/>
         </label>
 
        
@@ -32,10 +32,10 @@ const ProjectForm = ({ onAddProject }) => {
         <label>
           Description
           <textarea value={description} onChange={(event) => setDescription(event.target.value)}
-          className='bg-white border text-left border-black resize-none'/>
+          className='bg-white border text-left border-black rounded-md resize-none w-full px-2 py-1'/>
         </label>
 
-        <button type="submit" className="bg-cyan-300  text-black rounded-md px-3 py-1 w-full">Add</button>
+        <button type="submit" className="bg-cyan-300 hover:bg-cyan-400 text-black font-semibold rounded-md px-3 py-2 w-full">Add</button>
       </form>
     </div>
   )
